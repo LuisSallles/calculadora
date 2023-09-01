146398ef51206c9e42186c93f725ccff0e2752a1
Luis Salles <107654852+LuisSallles@users.noreply.github.com>
2023-09-01 11:20:01 -0300

    training

--- javascript.js
+++ javascript.js
@@ -1,13 +1,16 @@
+// Inserting numbers on the screen
 function insert(num) {
-    var number = document.getElementById('calculator-result').innerHTML 
-    document.getElementById('calculator-result').innerHTML = number + num
+    var numRes = document.getElementById('calculator-result').innerHTML
+    document.getElementById('calculator-result').innerHTML = num + numRes
 }
 
+// Reset all numbers
 function reset() {
-    window.document.getElementById('calculator-result').innerHTML = ''
+    document.getElementById('calculator-result').innerHTML = ''
 }
 
-function back() {
-    var res = document.getElementById('calculator-result').innerHTML
-    document.getElementById('calculator-result') 
+// Clean only a number on the screen
+function del() {
+    var reset = document.getElementById('calculator-result').innerHTML
+    document.getElementById('calculator-result').innerHTML = reset.substring(0, reset.length -1)
 }
